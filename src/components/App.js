9b8e8197f72a132061ec529c10f61171d4dbbf0c
Suraj Pulloor <thegamer600@gmail.com>
2021-06-13 23:48:53 +0530

    Added a proper structure for the project

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <hi>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum quo fugiat velit aliquam, harum sequi blanditiis vitae unde temporibus repellat, iste possimus ad debitis laborum asperiores quia! Ipsum, quam assumenda.</hi>
+    </div>
+  );
+}
+
+export default App;
