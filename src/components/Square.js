27d0449c2f6a962167986cfcb72cfec590ec6036
Suraj Pulloor <thegamer600@gmail.com>
2021-06-26 11:57:50 +0530

    Added the &nbsp; char entity when the square doesn't have a piece yet. This fixes the layout of the board

--- src/components/Square.js
+++ src/components/Square.js
@@ -6,7 +6,7 @@ function Square(props) {
     return (
 
         <span className="square" onClick={props.onClick}>
-            {props.sign}
+            {props.sign ? props.sign : String.fromCharCode(160)}
         </span>
         
     );
