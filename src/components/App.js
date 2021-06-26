7c591388c2f7d39edea18f9a908c88f1a176d520
Suraj Pulloor <thegamer600@gmail.com>
2021-06-26 11:56:01 +0530

    Added a description for shortcuts

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,14 @@ function App() {
   return (
     <div className="container">
       <Board />  
+      <h3>
+        You can use the following keyboard shortcuts
+        
+        <ul>
+          <li>"s" to start/reset the game</li>
+          <li>"0-9" to play the moves</li>
+        </ul>
+      </h3>
     </div>
   );
 }
